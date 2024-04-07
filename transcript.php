6127baf1b216a00911b12736e73c34a11bef65c0
Shadakur Rahman Tamgid <tamgid311@gmail.com>
2024-04-07 11:25:53 +0600

    Creating Initial Interface

---
+++ transcript.php
@@ -0,0 +1,315 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Academic Transcript</title>
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Abel&family=Exo+2:ital,wght@0,100..900;1,100..900&family=IBM+Plex+Mono:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;1,100;1,200;1,300;1,400;1,500;1,600;1,700&family=La+Belle+Aurore&family=Merriweather:ital,wght@0,300;0,400;0,700;0,900;1,300;1,400;1,700;1,900&family=Oswald:wght@200..700&family=Roboto+Condensed:ital,wght@0,100..900;1,100..900&family=Roboto+Slab:wght@100..900&family=Source+Code+Pro:ital,wght@0,200..900;1,200..900&display=swap" rel="stylesheet">
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.css" integrity="sha512-3pIirOrwegjM6erE5gPSwkUzO+3cTjpnV9lexlNZqvupR64iZBnOOTiiLPb9M36zpMScbmUNIcHUqKD47M719g==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+    <link href="css/carousel.css" rel="stylesheet">
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.js" integrity="sha512-VEd+nq25CkR676O+pLBnDW09R7VQX9Mdiij052gVCp5yVH3jGtH70Ho/UUv4mJDsEdTvqRCFZg0NKGiojGnUCw==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+
+    <!-- font style -->
+    <style>
+        .a4-size {
+            width: 265mm;
+            height: 350mm;
+            margin: 0 auto;
+            padding: 20mm;
+            box-sizing: border-box;
+        }
+
+        .page-content {
+            position: relative;
+            width: 100%;
+            height: 100%;
+        }
+
+        .roboto-condensed-heading {
+            font-family: "Roboto Condensed", sans-serif;
+            font-weight: 500;
+            font-style: normal;
+        }
+
+        .roboto-slab-desc {
+            font-family: "Roboto Slab", serif;
+            font-optical-sizing: auto;
+            font-weight: 500;
+            font-style: normal;
+            font-style: italic;
+        }
+
+        .abel-regular {
+            font-family: "Abel", sans-serif;
+            font-weight: 400;
+            font-style: normal;
+            font-style: italic;
+        }
+
+        .exo-2-paragraph {
+            font-family: "Exo 2", sans-serif;
+            font-optical-sizing: auto;
+            font-weight: 500;
+            font-style: normal;
+        }
+
+        .merriweather-regular {
+            font-family: "Merriweather", serif;
+            font-weight: 500;
+            font-style: normal;
+        }
+
+        .source-code {
+            font-family: "Source Code Pro", monospace;
+            font-optical-sizing: auto;
+            font-weight: 500;
+            font-style: normal;
+        }
+
+        .link {
+            color: cadetblue;
+            text-decoration: underline;
+        }
+
+        .image-container {
+            width: 100px;
+            height: 100px;
+            border-radius: 50%;
+            overflow: hidden;
+            margin: 0 auto 10px;
+        }
+
+        .circular-image {
+            width: 100%;
+            height: auto;
+            display: block;
+        }
+
+        table {
+            width: 100%;
+            border-collapse: collapse;
+            font-weight: 500;
+        }
+
+        td {
+            border: 2px solid black;
+            text-align: center;
+        }
+
+        .title {
+            font-weight: 450;
+        }
+
+        .inter {
+            font-weight: 400;
+        }
+
+        .subject {
+            text-align: left;
+            margin-left: 10;
+        }
+
+        .additional {
+            text-align: left;
+        }
+
+        .table_row {
+            height: 50px;
+        }
+    </style>
+
+</head>
+
+<body class="a4-size">
+    <div class="page-content">
+        <div class="px-4 py-4 text-center">
+            <h4 class="roboto-condensed-heading text-center">BOARD OF INTERMEDIATE AND SECONDARY EDUCATION, CHITTAGONG.</h4>
+            <h5 class="exo-2-paragraph text-center">BANGLADESH</h5>
+            <h5 class="merriweather-regular text-center">Higher Secondary Certificate Examination, 2020</h5>
+        </div>
+        <div class="row g-5 row-cols-1 row-cols-lg-3 text-center">
+            <div class="feature col">
+                <p class="source-code text-center">Serial No. DBHT 20 0200536</p>
+                <p class="source-code text-center">DBCH 20289880</p>
+            </div>
+            <div class="feature col">
+                <div class="image-container">
+                    <img src="logo.png" alt="Image" class="circular-image">
+                </div>
+                <h5 class="link">ACADEMIC TRANSCRIPT</h5>
+            </div>
+            <div class="feature col">
+                <table>
+                    <tbody>
+                        <tr>
+                            <td>Letter Grade</td>
+                            <td>Class Interval (%)</td>
+                            <td>Grade Point</td>
+                        </tr>
+                        <tr>
+                            <td>A+</td>
+                            <td>80-100</td>
+                            <td>5</td>
+                        </tr>
+                        <tr>
+                            <td>A</td>
+                            <td>70-79</td>
+                            <td>4</td>
+                        </tr>
+                        <tr>
+                            <td>A-</td>
+                            <td>60-69</td>
+                            <td>3.5</td>
+                        </tr>
+                        <tr>
+                            <td>B</td>
+                            <td>50-59</td>
+                            <td>3</td>
+                        </tr>
+                        <tr>
+                            <td>C</td>
+                            <td>40-49</td>
+                            <td>2</td>
+                        </tr>
+                        <tr>
+                            <td>D</td>
+                            <td>33-39</td>
+                            <td>1</td>
+                        </tr>
+                        <tr>
+                            <td>F</td>
+                            <td>0-32</td>
+                            <td>0</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+        <div class="row g-5 row-cols-1 row-cols-lg-3 text-left">
+            <div class="feature col">
+                <div class="title">
+                    <p>Name of Student&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;:</p>
+                    <p>Father's Name&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;:</p>
+                    <p>Mother's Name&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;:</p>
+                    <p>Name of Institution&nbsp;&nbsp;&nbsp;:</p>
+                    <p>Name of Centre&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;:</p>
+                </div>
+            </div>
+            <div class="feature col">
+                <p class="roboto-slab-desc">Shadakur Rahman Tamgid</p>
+                <p class="roboto-slab-desc">Gias Uddin</p>
+                <p class="roboto-slab-desc">Marjina Begum</p>
+                <p class="abel-regular">Hazera Taju Degree College [ 1080 ]</p>
+                <p class="abel-regular">Chittagong College [ 3420 ]</p>
+            </div>
+        </div>
+        <div class="row g-5 row-cols-1 row-cols-lg-4 text-left">
+            <div class="feature col">
+                <div class="title">
+                    <p>Roll No.&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;:</p>
+                    <p>Group&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;:</p>
+                </div>
+            </div>
+            <div class="feature col">
+                <p class="roboto-slab-desc">214931</p>
+                <p class="roboto-slab-desc">Science</p>
+            </div>
+            <div class="feature col">
+                <div class="title">
+                    <p>Registration No.&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;:</p>
+                    <p>Type of Student&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;:</p>
+                </div>
+            </div>
+            <div class="feature col">
+                <p class="roboto-slab-desc">1414711971</p>
+                <p class="roboto-slab-desc">Regular</p>
+            </div>
+        </div>
+        <table>
+            <tbody>
+                <tr>
+                    <td>SL. No.</td>
+                    <td>Name of Subjects</td>
+                    <td>Letter Grade</td>
+                    <td>Grade Point</td>
+                    <td>GPA <p class="inter">(without additional subject)</p>
+                    </td>
+                    <td>GPA</td>
+                </tr>
+                <tr class="table_row">
+                    <td>1</td>
+                    <td class="subject">Bangla</td>
+                    <td>A+</td>
+                    <td>5</td>
+                    <td rowspan="6">4.50</td>
+                    <td rowspan="8">5.00</td>
+                </tr class="table_row">
+                <tr class="table_row">
+                    <td>2</td>
+                    <td class="subject">English</td>
+                    <td>A</td>
+                    <td>4</td>
+                </tr>
+                <tr class="table_row">
+                    <td>3</td>
+                    <td class="subject">Information & Communication Technology</td>
+                    <td>A+</td>
+                    <td>5</td>
+                </tr>
+                <tr class="table_row">
+                    <td>4</td>
+                    <td class="subject">Physics</td>
+                    <td>A</td>
+                    <td>4</td>
+                </tr>
+                <tr class="table_row">
+                    <td>5</td>
+                    <td class="subject">Chemistry</td>
+                    <td>A+</td>
+                    <td>5</td>
+                </tr>
+                <tr class="table_row">
+                    <td>6</td>
+                    <td class="subject">Higher Mathematics</td>
+                    <td>A+</td>
+                    <td>5</td>
+                </tr>
+                <tr class="table_row">
+                    <td colspan="5" class="additional">Additional Subject :</td>
+                </tr>
+                <tr class="table_row">
+                    <td>7</td>
+                    <td class="subject">Biology</td>
+                    <td>A+</td>
+                    <td>5</td>
+                    <td>3.0</td>
+                </tr>
+            </tbody>
+        </table>
+        <div class="row g-5 row-cols-1 row-cols-lg-2" style="margin-top: 5px;">
+            <div class="feature col text-left">
+                <div class="title">
+                    <p>Date of Publication of Results :</p>
+                </div>
+            </div>
+            <div class="feature col text-center">
+                <p class="abel-regular">Controller of Examinations</p>
+            </div>
+        </div>
+    </div>
+</body>
+
+
+</html>
